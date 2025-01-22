f24b8035ecc4580db235dbb329f59bb31ea4d83f
smmonino <smmoninog01@iesalbarregas.es>
2025-01-22 17:48:00 +0100

    Cambio en linea de abajo

--- Hola_mundo.java
+++ Hola_mundo.java
@@ -1,10 +1,10 @@
 public class Hola_mundo{
 
 	public static void main(String[] args){
-
+		//añadido en test_cvs1
 		System.out.println("Hola_mundo");
 		System.out.println("Adiós");
-	
+		
 	}
 
 }
