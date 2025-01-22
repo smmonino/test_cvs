2e5efcab7756b31448c2aa53f0f2c0e5b3f3c14e
smmonino <smmoninog01@iesalbarregas.es>
2025-01-22 17:30:30 +0100

    Varios cambios
    1.Añadir la clase del Hola_mundo
    2.Modificar Hola_mundo
    3.Añadir la clase de Nuevo
    4.Añadir el nuevo fichero Nuevo.java
    5.Añadir el fichero hola_mundo.txt de prueba vacio

--- Hola_mundo.java
+++ Hola_mundo.java
@@ -3,6 +3,7 @@ public class Hola_mundo{
 	public static void main(String[] args){
 
 		System.out.println("Hola_mundo");
+		System.out.println("Adiós");
 	
 	}
 
---
+++ Nuevo.java
@@ -0,0 +1,7 @@
+public class Nuevo{
+
+	public static void main(String[] args){
+		int n=10;
+		System.out.println(n);
+	}
+}
